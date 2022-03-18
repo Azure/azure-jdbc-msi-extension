1d76724fe7177527b1161a327612c8238379f37c
Manfred Riem <manfred.riem@microsoft.com>
2022-03-18 10:26:39 -0600

    Added PostgreSQL prototype

---
+++ postgresql/src/main/java/com/azure/postgresql/auth/plugin/AzurePostgresqlMSIAuthenticationPlugin.java
@@ -0,0 +1,84 @@
+package com.azure.postgresql.auth.plugin;
+
+import com.azure.core.credential.AccessToken;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.credential.TokenRequestContext;
+import com.azure.identity.AzureCliCredentialBuilder;
+import com.azure.identity.ChainedTokenCredentialBuilder;
+import com.azure.identity.ManagedIdentityCredentialBuilder;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Properties;
+import org.postgresql.plugin.AuthenticationPlugin;
+import org.postgresql.plugin.AuthenticationRequestType;
+import org.postgresql.util.PSQLException;
+import static org.postgresql.util.PSQLState.INVALID_PASSWORD;
+
+/**
+ * The Authentication plugin that enables Azure AD managed identity support.
+ */
+public class AzurePostgresqlMSIAuthenticationPlugin implements AuthenticationPlugin {
+
+    /**
+     * Stores the access token.
+     */
+    private AccessToken accessToken;
+
+    /**
+     * Stores the properties.
+     */
+    private Properties properties;
+
+    /**
+     * Constructor.
+     */
+    public AzurePostgresqlMSIAuthenticationPlugin() {
+    }
+
+    /**
+     * Constructor with properties.
+     *
+     * @param properties the properties.
+     */
+    public AzurePostgresqlMSIAuthenticationPlugin(Properties properties) {
+        this.properties = properties;
+        
+        if (!properties.containsKey("user")) {
+            properties.setProperty("user", new ManagedIdentityCredentialBuilder().build().getClientId());
+        }
+    }
+
+    /**
+     * Get the password.
+     * 
+     * @param art the authentication request type.
+     * @return the password.
+     * @throws PSQLException when an error occurs. 
+     */
+    @Override
+    public char[] getPassword(AuthenticationRequestType art) throws PSQLException {
+        char[] password;
+
+        String username = properties.getProperty("user");
+
+        ArrayList<TokenCredential> credentials = new ArrayList<>();
+        credentials.add(new ManagedIdentityCredentialBuilder()
+                .clientId(username).build());
+        credentials.add(new AzureCliCredentialBuilder().build());
+        TokenCredential credential = new ChainedTokenCredentialBuilder().addAll(credentials).build();
+
+        TokenRequestContext request = new TokenRequestContext();
+        ArrayList<String> scopes = new ArrayList<>();
+        scopes.add("https://ossrdbms-aad.database.windows.net");
+        request.setScopes(scopes);
+        accessToken = credential.getToken(request).block(Duration.ofSeconds(30));
+
+        if (accessToken != null) {
+            password = accessToken.getToken().toCharArray();
+        } else {
+            throw new PSQLException("Unable to acquire access token", INVALID_PASSWORD);
+        }
+
+        return password;
+    }
+}
---
+++ postgresql/src/test/java/test/JDBCTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.junit.Test;
+
+/**
+ * A test that can be used to verify that managed identity is working with your
+ * MySQL JDBC connection.
+ */
+public class JDBCTest {
+
+    /**
+     * Test connection.
+     */
+    @Test
+    public void testConnection() {
+        /*
+         * &user=manriem@microsoft.onmicrosoft.com@postgres-msi
+         */
+        main(new String[]{System.getProperty("url")});
+    }
+
+    /**
+     * Main method.
+     *
+     * @param arguments the arguments.
+     */
+    public static void main(String[] arguments) {
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(arguments[0]);
+
+            if (connection != null) {
+                System.out.println("Successfully connected.");
+                System.out.println(connection.isValid(10));
+            } else {
+                System.out.println("Failed to connect.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+    }
+}
