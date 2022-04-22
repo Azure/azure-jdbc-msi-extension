76ed2881dfc6e2ead894b2bf7b72f2fb86ec7bfe
felipmiguel <fmiguel@microsoft.com>
2022-04-22 12:17:33 +0200

    mysql - drop user before creating

--- demo/deployment/mysql/mysql_create_user.sql
+++ demo/deployment/mysql/mysql_create_user.sql
@@ -1,5 +1,7 @@
 SET aad_auth_validate_oids_in_tenant = OFF;
 
+DROP USER '%%login_name%%'@'%';
+
 CREATE AADUSER '%%login_name%%' IDENTIFIED BY '%%user_id%%';
 grant all privileges on Db.* to '%%login_name%%'@'%';
 
