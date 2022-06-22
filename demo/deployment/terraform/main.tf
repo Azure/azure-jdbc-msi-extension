c0ab125275cef6de697af9f2fd731528adc89166
zhihaoguo <zhihaoguo@microsoft.com>
2022-06-22 09:31:00 +0800

    update hashicorp/azurerm to 3.9.0

--- demo/deployment/terraform/main.tf
+++ demo/deployment/terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.0.1"
+      version = "3.9.0"
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"
